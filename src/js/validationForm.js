a7a469c7d0462f4b5584ffb33da7d2e0b3c1658b
Alexandr Yakovlev <alexandr.yakovlev228@gmail.com>
2021-02-27 17:57:48 +0300

    добавил проект и изменил дизайн

--- src/js/validationForm.js
+++ src/js/validationForm.js
@@ -5,66 +5,21 @@ const validationForm = () => {
         name_exp: {
             required: true,
             minLength: 3,
-            name: true,
-            handlerWhenInvalidElement: function() {
-              const warning = document.querySelector('.works__list-item-form-warning-name');
-              
-              warning.classList.add('show');
-            },
-            handlerWhenValidElement: function() {
-              const warning = document.querySelector('.works__list-item-form-warning-name');
-              
-              warning.classList.remove('show');
-            }
+            name: true
         },
         email_exp: {
             required: true,
-            mail: true,
-            handlerWhenInvalidElement: function() {
-              const warning = document.querySelector('.works__list-item-form-warning-email');
-              
-              warning.classList.add('show');
-            },
-            handlerWhenValidElement: function() {
-              const warning = document.querySelector('.works__list-item-form-warning-email');
-              
-              warning.classList.remove('show');
-            }
+            mail: true
         },
         password_exp: {
             required: true,
             minLength: 6,
-            password: true,
-            handlerWhenInvalidElement: function() {
-              const warning = document.querySelector('.works__list-item-form-warning-password');
-              
-              warning.classList.add('show');
-            },
-            handlerWhenValidElement: function() {
-              const warning = document.querySelector('.works__list-item-form-warning-password');
-              
-              warning.classList.remove('show');
-            }
+            password: true
         },
         'repeat-password_exp': {
             required: true,
             repeatPassword: true,
-            repeatAt: 'password_exp',
-            handlerWhenInvalidElement: function() {
-              const warning = document.querySelector('.works__list-item-form-warning-repeat-password');
-              
-              warning.classList.add('show');
-            },
-            handlerWhenValidElement: function() {
-              const warning = document.querySelector('.works__list-item-form-warning-repeat-password');
-              
-              warning.classList.remove('show');
-            }
-        },
-        submit_exp: {
-            textWhenValid: 'Зарегистрироваться',
-            textWhenInvalid: 'Повторите попытку!',
-            button: true
+            repeatAt: 'password_exp'
         }
     }
 
